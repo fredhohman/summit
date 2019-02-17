f6cdce63eff1bf6f01607943b60b5d65598a0a11
Fred Hohman <fred.hohman@gmail.com>
2019-02-17 18:32:29 -0500

    class bar styling

--- js/class-sidebar.js
+++ js/class-sidebar.js
@@ -13,8 +13,9 @@ const layerChannelCounts = {
     'mixed5b': 1024
 }
 
-let layer = 'mixed4e'
+let layer = 'mixed4e';
 let selectedClass = 0;
+const numClassesInClassBar = 250;
 
 let leftInner = d3.select('#left')
     .append('div')
@@ -69,7 +70,7 @@ d3.json('./data/imagenet.json').then(function (data) {
                         return d3.descending(x.distanceFromQueryPoint, y.distanceFromQueryPoint);
                     })
                     // .filter(d => d.distanceFromQueryPoint < 2)
-                    .slice(0, 1000)
+                    .slice(0, numClassesInClassBar) // nearest n classes
                 )
             
             .enter()
@@ -92,21 +93,92 @@ d3.json('./data/imagenet.json').then(function (data) {
 
         classBarTexts.append('div')
             .classed('class-bar-text-accuracy', true)
-            .text(d => d.topOneAcc.toFixed(2))
+            .text(d => (100 * d.topOneAcc).toFixed(1) + '%')
 
-        classBarTexts.append('div')
+        let classBarHistograms = classBarTexts.append('div')
             .classed('class-bar-text-histogram', true)
-            .text('h')
+            // .text('h')
+
+        const accuracyMargin = { top: 2, right: 0, bottom: 2, left: 0 }
+        const accuracyWidth = 100 - accuracyMargin.left - accuracyMargin.right // 100 from flex-basis width of class-bar-text-accuracy
+        const accuracyHeight = 25 - accuracyMargin.top - accuracyMargin.bottom // 100 from flex-basis width of class-bar-text-accuracy
+
+        classBarHistograms
+            .append('svg')
+            .attr("width", accuracyWidth + accuracyMargin.left + accuracyMargin.right)
+            .attr("height", accuracyHeight + accuracyMargin.top + accuracyMargin.bottom)
+            // .style('border', '1px solid #eeeeee')
+            // .attr('id', d => 'accuracy-' + d.synset)
+            .append("g")
+            .attr("transform", "translate(" + accuracyMargin.left + "," + accuracyMargin.top + ")")
+            .attr('id', d => 'accuracy-' + d.synset)
+
+        function makeAccuracyHistogram(c) {
+            let accuracySVG = d3.select('#accuracy-' + c.synset)
+            // let accuracyG = d3.select('#accuracy-g-' + c.synset)
+
+            let accuracyX = d3.scaleLinear()
+                .domain(d3.extent(c.accuracies)).nice()
+                .range([0, accuracyWidth])
+
+            let bins = d3.histogram()
+                .domain(accuracyX.domain())
+                .thresholds(accuracyX.ticks(20))
+                (c.accuracies)
+
+            let accuracyY = d3.scaleLinear()
+                .domain([0, d3.max(bins, d => d.length)]).nice()
+                // .domain([0, 1300])
+                .range([accuracyHeight, 0])
+
+            let xAxis = accuracySVG
+                .append('g')
+                .attr("transform", "translate(0," + accuracyHeight + ")")
+                .classed('accuracy-x-axis', true)
+                .call(d3.axisBottom(accuracyX).tickSizeOuter(0).ticks(0))
+            // .call(g => g.append("text")
+            //     .attr("x", accuracyWidth - accuracyMargin.right)
+            //     .attr("y", -4)
+            //     .attr("fill", "#000")
+            //     .attr("font-weight", "bold")
+            //     .attr("text-anchor", "end")
+            //     .text('accuracy')
+            //     )
+
+            // let yAxis = accuracySVG
+            //     .append('g')
+            //     // .attr("transform", "translate(" + accuracyMargin.left + ",0)")
+            //     .call(d3.axisLeft(accuracyY).ticks(0))
+            // .call(g => g.select(".domain").remove())
+            // // .call(g => g.select(".tick:last-of-type text").clone()
+            // //     .attr("x", 4)
+            // //     .attr("text-anchor", "start")
+            // //     .attr("font-weight", "bold")
+            // //     .text('count'))
+
+            accuracySVG
+                .append("g")
+                .selectAll("rect")
+                .data(bins)
+                .enter().append("rect")
+                .classed('accuracy-bar', true)
+                .attr("x", d => accuracyX(d.x0) + 1)
+                .attr("width", d => Math.max(0, accuracyX(d.x1) - accuracyX(d.x0) - 1))
+                .attr("y", d => accuracyY(d.length))
+                .attr("height", d => accuracyY(0) - accuracyY(d.length));
+        }
+
+        for (let i = 0; i < data.length; i++) {
+            makeAccuracyHistogram(data[i])
+        }
 
         let classBarBars = classBars.append('div')
             .classed('class-bar-bar-wrapper', true)
         
         let classBarBarsScale = d3.scaleLinear()
             .domain([0, 1]) // cosine similarity
-            .range([0, 100])
+            .range([0, 100]) // div width percentage
         
-        console.log(d3.extent(data, d => d.distanceFromQueryPoint))
-
         classBarBars.append('div')
             .classed('class-bar-bar-data', true)
             .style('width', d => classBarBarsScale(d.distanceFromQueryPoint) + '%')
