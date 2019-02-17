affd0bdd277fc86f85d6ae755c11f72a206bbd52
Fred Hohman <fred.hohman@gmail.com>
2019-02-17 13:22:50 -0500

    class sidebar sorts and filters data based on threshold for closeness, maybe switch to total count? better class sidebar styling

--- js/class-sidebar.js
+++ js/class-sidebar.js
@@ -28,12 +28,54 @@ d3.json('./data/imagenet.json').then(function (data) {
 
 
     function makeClassBars(data, selectedClass) {
-        leftInnerClassBarWrapper.selectAll('.class-bar')
-            .data(data.filter(d => d.label < 20))
+
+        let classBars = leftInnerClassBarWrapper.selectAll('.class-bar')
+            .data(data
+                    .sort(function (x, y) {
+                        return d3.ascending(x.distanceFromQueryPoint, y.distanceFromQueryPoint);
+                    })
+                    .filter(d => d.distanceFromQueryPoint < 2)
+                )
+            
             .enter()
             .append('div')
             .classed('class-bar', true)
+
+        let classBarTexts = classBars.append('div')
+            .classed('class-bar-text-wrapper', true)
+        
+        classBarTexts.append('div')
+            .classed('class-bar-text-name', true)
             .text(d => d.name)
+
+        classBarTexts.append('div')
+            .classed('class-bar-text-instances', true)
+            .text(d => d.numOfInstances)
+
+        classBarTexts.append('div')
+            .classed('class-bar-text-accuracy', true)
+            .text(d => d.topOneAcc.toFixed(2))
+
+        classBarTexts.append('div')
+            .classed('class-bar-text-histogram', true)
+            .text('h')
+
+        let classBarBars = classBars.append('div')
+            .classed('class-bar-bar-wrapper', true)
+        
+        let classBarBarsScale = d3.scaleLinear()
+            .domain(d3.extent(data, d => d.distanceFromQueryPoint))
+            .range([100, 0])
+        
+        console.log(d3.extent(data, d => d.distanceFromQueryPoint))
+
+        classBarBars = classBarBars.append('div')
+            .classed('class-bar-bar-background', true)
+            .append('div')
+            .classed('class-bar-bar-data', true)
+            .style('width', d => classBarBarsScale(d.distanceFromQueryPoint) + '%')
+
+        
 
     }
     makeClassBars(data)
