ed640de86b9fc8cc37fcb02009641b01e2fa7d5e
Fred Hohman <fred.hohman@gmail.com>
2019-02-19 14:03:21 -0500

    init feature vis css and dag.js testing

--- js/dag.js
+++ js/dag.js
@@ -6,9 +6,19 @@ let rightInnerOptions = d3.select('#right-inner-options')
 
 let rightInnerEmbeddingWrapper = d3.select('#right-inner-tree-wrapper')
 
-rightInnerEmbeddingWrapper
+let test = rightInnerEmbeddingWrapper
+    .append('div')
+    // .classed('dataset-examples', true)
+test
+    .append('style')
+    .text('.sprite {background-image: url(../data/feature-vis/mixed4b-00310--mixed4b-00319.jpg)}')
+
+test
     .append('div')
         .attr('id', 'test-div')
-    .append('span')
-        .attr('id', 'test-span')
+        .classed('sprite', true)
+        .classed('channel-diversity-0', true)
+        .classed('index-0', true)
+    // .append('span')
+        // .attr('id', 'test-span')
 
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import * as style from "../css/style.css";
 import * as styleHeader from "../css/header.css";
-import * as topBar from './top-bar'
-import * as classSidebar from './class-sidebar'
-
-
+import * as featureVis from "../css/feature-vis.css";
+import * as classSidebar from './class-sidebar';
+import * as dag from './dag';
+import * as topBar from './top-bar';
