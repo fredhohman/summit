6aebf10f60415d7bc7fe3de18de51eff6c8597d4
Fred Hohman <fred.hohman@gmail.com>
2019-02-22 00:25:36 -0500

    add zoom to dag, get clip path on svg image

--- js/dag.js
+++ js/dag.js
@@ -1,12 +1,13 @@
 import * as d3 from "d3"
 
+
 let rightInner = d3.select('#right-inner')
 
 let rightInnerOptions = d3.select('#right-inner-options')
 
-let rightInnerEmbeddingWrapper = d3.select('#right-inner-tree-wrapper')
+let rightInnerDagWrapper = d3.select('#right-inner-dag-wrapper')
 
-let test = rightInnerEmbeddingWrapper
+let test = rightInnerDagWrapper
     .append('div')
     // .classed('dataset-examples', true)
 test
@@ -17,8 +18,80 @@ test
     .append('div')
         .attr('id', 'test-div')
         .classed('sprite', true)
-        .classed('channel-diversity-0', true)
+        // .classed('channel-diversity-0', true)
         .classed('index-0', true)
+        // .on('mouseover', () => {
+        //     // d3.select('#test-div')
+        //     // .transition()
+        //     // .duration(1000)
+        //     // .style('background-position-x', '-441px')
+        //     // .transition()
+        //     // .duration(1000)
+        //     // .style('background-position-x', '-588px')
+        // })
+        // .on('mouseout', () => {
+        // })
     // .append('span')
         // .attr('id', 'test-span')
 
+
+
+const dagMargin = ({ top: 40, right: 40, bottom: 40, left: 40 })
+const dagWidth = 700 - dagMargin.left - dagMargin.right
+const dagHeight = 700 - dagMargin.top - dagMargin.bottom // 790 based on laptop screen height
+let k = 1; // dag zoom scale
+
+let zoom = d3.zoom()
+    .scaleExtent([.5, 90])
+    .extent([[0, 0], [dagWidth, dagHeight]])
+    .on("zoom", zoomed);
+
+function zoomed() {
+    d3.select('#dagG').attr("transform", d3.event.transform);
+    console.log(d3.event.transform)
+}
+
+let dagSVG = rightInnerDagWrapper
+    .append('svg')
+    .attr('viewBox', '0 0 ' + (dagWidth + dagMargin.left + dagMargin.right) + ' ' + (dagHeight + dagMargin.top + dagMargin.bottom))
+    .attr('width', '100%')
+    .style('border', '1px solid #eeeeee') // for debugging
+    .attr('id', 'dag')
+    // .call(zoom)
+
+let zoomRect = dagSVG.append("rect")
+    .attr("width", dagWidth)
+    .attr("height", dagHeight)
+    .style("fill", "none")
+    .style("pointer-events", "all")
+    .attr('transform', 'translate(' + dagMargin.left + ',' + dagMargin.top + ')')
+    .call(zoom);
+
+let dagG = dagSVG
+    .append("g")
+    .attr("transform", "translate(" + dagMargin.left + "," + dagMargin.top + ")")
+    .attr('id', 'dagG')
+
+let dagDefs = dagSVG.append('defs')
+
+const fvWidth = 100
+const fvHeight = fvWidth
+dagDefs.append('clipPath')
+    .attr('id', 'fv-clip-path')
+    .append('rect')
+    .attr('x', 0)
+    .attr('y', 0)
+    .attr('width', fvWidth)
+    .attr('height', fvHeight)
+    .attr('rx', 10)
+    .attr('ry', 10)
+
+dagG.append('image')
+    .attr('x', 0)
+    .attr('y', 0)
+    .attr('width', fvWidth)
+    .attr('height', fvHeight)
+    .attr('xlink:href', '../data/feature-vis/mixed4d-376-channel.png')
+    .classed('fv-channel', true)
+    .attr('clip-path', 'url(#fv-clip-path)')
+    .attr("transform", "translate(400, 100)")
