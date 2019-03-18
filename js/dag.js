e6dc74535e5639f170eb958150fd1185f807e9b9
Fred Hohman <fred.hohman@gmail.com>
2019-03-17 17:49:24 -0700

    no more hard code

--- js/dag.js
+++ js/dag.js
@@ -224,9 +224,9 @@ export function dagVIS(selectedClass) {
     console.log('dagVIS', selectedClass)
     
     // d3.json('./data/dag/dag-270.json').then(function (dag) {
-    d3.json('./data/dag/dag-270-unified.json').then(function (dag) {
+    // d3.json('./data/dag/dag-270-unified.json').then(function (dag) {
     // d3.json('./data/dag/dag-' + selectedClass['target_class'] + '.json').then(function (dag) {
-    // d3.json('./data/dag/dag-' + selectedClass['target_class'] + '-unified.json').then(function (dag) {
+    d3.json('./data/dag/dag-' + selectedClass['target_class'] + '-unified.json').then(function (dag) {
         console.log(dag);
 
         let tempMins = []
