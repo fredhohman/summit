7e2f95ee62920e9955d52e9818a547df505ce995
Fred Hohman <fred.hohman@gmail.com>
2019-03-23 09:33:54 -0400

    remove old feature-vis.css

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 import * as style from "../css/style.css";
 import * as styleHeader from "../css/header.css";
-import * as featureVis from "../css/feature-vis.css";
 import * as search from "../css/search.css";
 import * as dag from './dag';
 // import * as classSidebar from './class-sidebar';
