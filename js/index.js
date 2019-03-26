0102f60124b957be85d8b53efe12e61ba49d9283
Fred Hohman <fred.hohman@gmail.com>
2019-03-26 18:14:08 -0400

    dag -> attribution graph

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import * as style from "../css/style.css";
 import * as styleHeader from "../css/header.css";
 import * as search from "../css/search.css";
-import * as dag from './dag';
+import * as ag from './attribution-graph';
 // import * as classSidebar from './class-sidebar';
