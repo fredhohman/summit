a3a4ca493117b8cbfabbd9f1afce8273bf0f387f
Fred Hohman <fred.hohman@gmail.com>
2019-03-28 23:21:03 -0400

    automatic data folder move when building

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
-echo 'Did you move the data/ folder out of dist/ (y/n)?'
-read answer
-if [ "$answer" != "${answer#[Yy]}" ] ;then
-    echo 'Yes'
-    webpack --env.dataURL='github' --mode production
-else
-    echo 'No'
-    echo 'Please move data/ folder!'
-fi
+echo 'Moving local data/ folder out of dist/'
+mv dist/data/ .
+echo 'Building with webpack'
+webpack --env.dataURL='github' --mode production
+echo 'Moving local data/ back to dist/'
+mv data/ dist/
+
+# echo 'Did you move the data/ folder out of dist/ (y/n)?'
+# read answer
+# if [ "$answer" != "${answer#[Yy]}" ] ;then
+#     echo 'Yes'
+#     webpack --env.dataURL='github' --mode production
+# else
+#     echo 'No'
+#     echo 'Please move data/ folder!'
+# fi
