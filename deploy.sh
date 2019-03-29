4a04bb010ddf8c83925bd698781e28ae2a188423
Fred Hohman <fred.hohman@gmail.com>
2019-03-28 23:27:28 -0400

    add build and deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,8 @@ mv dist/data/ .
 echo 'Building with webpack'
 webpack --env.dataURL='github' --mode production
 
+echo 'Deploying to gh-pages'
+gh-pages -d dist
+
 echo 'Moving local data/ back to dist/'
 mv data/ dist/
