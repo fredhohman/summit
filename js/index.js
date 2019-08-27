7120a10973f00bc8c2a3277cb51088f697e4f6fb
Fred Hohman <fred.hohman@gmail.com>
2019-08-27 19:35:09 -0400

    init post

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import * as style from "../css/style.css";
 import * as styleHeader from "../css/header.css";
 import * as search from "../css/search.css";
+import * as post from "../css/post.css";
 import * as ag from './attribution-graph';
 // import * as classSidebar from './class-sidebar';
